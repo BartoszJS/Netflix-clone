f2dff54436e8d72399917203c258fa68024fddb2
BartoszJS <bartosz290300@wp.pl>
2023-01-29 00:49:09 +0100

    43%

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { getProducts, Product } from "@stripe/firestore-stripe-payments";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -6,12 +7,21 @@ import { modalState } from "../atoms/modalAtom";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
+import Plans from "../components/Plans";
 import Row from "../components/Row";
 import useAuth from "../hooks/useAuth";
+import useSubscription from "../hooks/useSubscription";
+import payments from "../lib/stripe";
 import { Movie } from "../typings";
 import requests from "../utils/requests";
 
 export const getServerSideProps = async () => {
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true,
+  })
+    .then((res) => res)
+    .catch((error) => console.log(error.message));
   const [
     netflixOriginals,
     trendingNow,
@@ -42,6 +52,7 @@ export const getServerSideProps = async () => {
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
+      products,
     },
   };
 };
@@ -55,6 +66,7 @@ interface Props {
   horrorMovies: Movie[];
   romanceMovies: Movie[];
   documentaries: Movie[];
+  products: Product[];
 }
 
 const Home = ({
@@ -66,11 +78,17 @@ const Home = ({
   horrorMovies,
   romanceMovies,
   documentaries,
+  products,
 }: Props) => {
-  const { logout, loading } = useAuth();
-  const showModal = useRecoilValue(modalState);
+  console.log(products);
 
-  if (loading) return "Loading";
+  const { logout, loading, user } = useAuth();
+  const showModal = useRecoilValue(modalState);
+  const subscription = useSubscription(user);
+
+  if (loading || subscription === null) return null;
+
+  if (!subscription) return <Plans products={products} />;
 
   return (
     <div
